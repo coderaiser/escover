24a70646a30e89db73b113deb0603f6d9c916c08
coderaiser <coderaiser@cloudcmd.io>
2022-01-19 17:40:29 +0200

    feature: escover: add .. when to many lines to display

--- bin/escover.js
+++ bin/escover.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import {cli} from '../lib/cli/cli.js';
-import {read} from '../lib/coverage-file.js';
+import {read} from '../lib/coverage-file/coverage-file.js';
 
 cli({
     argv: process.argv,
