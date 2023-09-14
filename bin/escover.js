d6587ea6391a136d1ade5028d83f746da4fb2a94
coderaiser <coderaiser@cloudcmd.io>
2023-09-14 13:00:41 +0300

    chore: lint

--- bin/escover.js
+++ bin/escover.js
@@ -2,6 +2,7 @@
 
 import {cli} from '../lib/cli/cli.js';
 import {readCoverage} from '../lib/coverage-file/coverage-file.js';
+import process from 'node:process';
 import {readConfig} from '../lib/config.js';
 
 cli({
