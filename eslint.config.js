30ff84f629df60b32a44fa3cac1fde647a2a5160
coderiaser <coderaiser@cloudcmd.io>
2026-05-07 16:11:04 +0300

    feature: escover: get rid of mock-import

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,3 @@
-import {matchToFlat} from '@putout/eslint-flat';
 import {safeAlign} from 'eslint-plugin-putout';
-import {defineConfig} from 'eslint/config';
 
-export const match = {
-    '**/{fresh,register}.js': {
-        'n/no-unsupported-features/node-builtins': 'off',
-    },
-};
-
-export default defineConfig([safeAlign, matchToFlat(match)]);
+export default safeAlign;
