6765e2189b99eb3b6b5e44a154b482d077b1f20b
coderiaser <coderaiser@cloudcmd.io>
2026-05-15 20:20:07 +0300

    test: @escover/formatter-responsive: coverage

--- packages/formatter-responsive/lib/build-grouped-table.spec.js
+++ packages/formatter-responsive/lib/build-grouped-table.spec.js
@@ -63,3 +63,89 @@ test('formatter: responsive: getColor', (t) => {
     t.equal(result, expected);
     t.end();
 });
+
+test('buildGroupedTable: without percent column', (t) => {
+    const files = [{
+        filename: 'lib/index.js',
+        covered: false,
+        lines: [1, 2],
+        percentLines: 50,
+    }];
+    
+    const [first, second] = buildGroupedTable({
+        files,
+        showPercent: false,
+        linesColWidth: 20,
+        fileColWidth: 20,
+    });
+    
+    const expected = [
+        ['File', 'Uncovered Lines #s'],
+        [` ${stripAnsi(getColor(50)('index.js'))}`, stripAnsi(getColor(50)('1..2'))],
+    ];
+    
+    const normalized = [
+        first,
+        [
+            stripAnsi(second[0]),
+            stripAnsi(second[1]),
+        ],
+    ];
+    
+    t.deepEqual(normalized, expected);
+    t.end();
+});
+
+test('buildGroupedTable: truncate long file name', (t) => {
+    const files = [{
+        filename: 'lib/very-long-file-name-example.js',
+        covered: true,
+        lines: [],
+        percentLines: 100,
+    }, {
+        filename: 'src/another.js',
+        covered: true,
+        lines: [],
+        percentLines: 100,
+    }];
+    
+    const [, , [column]] = buildGroupedTable({
+        files,
+        showPercent: true,
+        linesColWidth: 20,
+        fileColWidth: 10,
+    });
+    
+    const normalized = stripAnsi(column);
+    const expected = ' ...le.js';
+    
+    t.equal(normalized, expected);
+    t.end();
+});
+
+test('buildGroupedTable: truncate long folder name', (t) => {
+    const files = [{
+        filename: '/very/long/path/to/folder/file.js',
+        covered: true,
+        lines: [],
+        percentLines: 100,
+    }, {
+        filename: '/another/folder/another.js',
+        covered: true,
+        lines: [],
+        percentLines: 100,
+    }];
+    
+    const [, [column]] = buildGroupedTable({
+        files,
+        showPercent: true,
+        linesColWidth: 20,
+        fileColWidth: 10,
+    });
+    
+    const result = stripAnsi(column);
+    const expected = '...older';
+    
+    t.match(result, expected);
+    t.end();
+});
